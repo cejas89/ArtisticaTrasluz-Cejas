01c4ba3de0857ea62086e32b6345e684a22e2a2c
Lucas Cejas <cejas_89@hotmail.com>
2022-08-22 08:04:38 -0300

    Modificando CONTADOR

--- src/components/ItemList/ItemCount.jsx
+++ src/components/ItemList/ItemCount.jsx
@@ -5,29 +5,49 @@ import ItemContainer from "./ItemContainer";
 const ItemCount = ({stock, initial, onAdd}) => {
     
     const [contador, setContador] = useState(0)
+    const [stockRestante, setStockRestante] = useState(stock) 
 
     const agregar = () => {
         if(contador<stock) {
-        setContador(contador + 1)
+            setContador(contador + 1)
+            setStockRestante((stock - 1) - contador)
+        
+        alert("has agregado con exito el articulo")
     };
     }
 
     
     const eliminar = () => {
         if(contador>0) {
+            alert("desea eliminar el articulo del carrito")
             setContador(contador - 1)
+            //setStockRestante(contador - 1)
     };
     }
 
+    
+
     return (
         <>
-                <p>Cantidad en stock = {stock}</p>
-                <p>Cantidad en stock = {initial}</p>
+                {
+                    stock>0 ?
+                    <p>Cantidad en stock = {stockRestante}</p>
+                    :
+                    <p>Cantidad en stock = {stockRestante}</p>
+                }
+                
+                
             <div>
                 <button onClick={agregar}>Agregar</button>
                 <button onClick={eliminar}>Eliminar</button>
-                <p>Los articulos agregados al carrito son: {onAdd}</p>
-                <p carrito = "5" >Contador = {contador} </p>
+
+                {
+                    contador>0 ?
+                    <p>Tienes {contador} articulos cargado al carrito</p>
+                    :
+                    <p>No tienes ningun articulo cargado al carrito</p>
+                }
+
             </div>
         </>
     )
