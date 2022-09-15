9cfde2fc64893da48a1c1bad671459fb44eb8b94
Lucas Cejas <cejas_89@hotmail.com>
2022-09-14 22:05:29 -0300

    avance con entrega 11

--- src/components/CartWidget/CartContainer.jsx
+++ src/components/CartWidget/CartContainer.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import {CartContext} from '../../context/CartContext'
 import { Cart } from './Cart';
+import { Link } from 'react-router-dom';
+
 
 export const CartContainer = () => {
 const {productCartList} = useContext(CartContext)
@@ -11,9 +13,15 @@ const {productCartList} = useContext(CartContext)
     <>
       {
         (productCartList.length === 0)
-        ? (<h1>No tienes articulos en el carrito</h1>)
-        :
-    (<div>
+     ?(<div>
+        <h1>No tienes articulos en el carrito</h1>
+        <Link to="/productos">
+      <button className='btn btn-outline-success'>
+        Regresa a ver los productos!!
+      </button>
+        </Link>
+      </div>)
+    :(<div>
     {productCartList.map(p => (
         <Cart key= {p.id} data = {p}/>
     ))}
